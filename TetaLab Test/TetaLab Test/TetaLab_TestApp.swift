5f2dd45f5af7f8b11b2889c617b374388d8ae0a5
TokarevychRoksolana <Liana721@ukr.net>
2022-05-23 22:23:05 +0200

    Initial commit

---
+++ TetaLab Test/TetaLab Test/TetaLab_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TetaLab_TestApp.swift
+//  TetaLab Test
+//
+//  Created by Liana Tokarevych on 23.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct TetaLab_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
